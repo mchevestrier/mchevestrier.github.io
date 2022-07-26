9bd63049e013db6b35e7129f4d01b69a730979d6
mchevestrier <103906265+mchevestrier@users.noreply.github.com>
2022-07-26 09:39:22 +0200

    Add Kubernetes

--- components/footer.jsx
+++ components/footer.jsx
@@ -6,9 +6,11 @@ export default function Footer() {
     <footer className="px-4 pt-20 pb-6 text-center">
       <span>
         Made with
+        {' '}
         <span className="text-red-500">❤</span>
         {' '}
         by
+        {' '}
         <Link href="/" passHref><a className="underline">Martin Chevestrier</a></Link>
       </span>
       <Socials />
